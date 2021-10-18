02aa9574fcc27e55fa53d6dcbe4ddfe737cd2c6c
Glauco Villas Boas <glaucovillasboas@pop-os.localdomain>
2021-10-18 05:38:26 -0300

    CRUD categories created

--- src/app.js
+++ src/app.js
@@ -1,27 +1,61 @@
 import express from 'express';
 import connection from './database/database.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.get('/teste', async (req, res) => {
-
+app.get('/categories', async (req, res) => {
   try {
-    const products = await connection.query(
-      `SELECT * FROM categories;`
-    );
+    const categories = await connection.query('SELECT * FROM categories;');
 
-    res.send(products.rows);
-  } catch (err) {
-    console.error(err);
+    res.status(200).send(categories.rows);
+  } catch {
+    res.sendStatus(500);
   }
 });
 
 
+app.post('/categories', async (req, res) => {
+  try {
+    const { name } = req.body;
+    console.log(req);
+    if (!name) {
+      res.sendStatus(400);
+      return;
+    }
+
+    const categories = await connection.query(
+      "SELECT * FROM categories WHERE name = $1", [name]
+    );
+
+    if (categories.rows[0]) {
+      res.sendStatus(409);
+    } else {
+      await connection.query(
+        "INSERT INTO categories (name) VALUES ($1)",
+        [name]
+      );
+      res.sendStatus(201);
+    }
+
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
 app.listen(4000, () => {
   console.log('Server is listening on port 4000.');
 });
+
+
+
+
+
+
+
+
 
 /* 
 app.get('/products', async (req, res) => {
