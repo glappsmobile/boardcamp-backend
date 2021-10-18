ff12f820f64493cdb81352b9e3a91ba3723c0ca8
Glauco Villas Boas <glaucovillasboas@pop-os.localdomain>
2021-10-18 17:27:59 -0300

    refactored

--- src/app.js
+++ src/app.js
@@ -409,34 +409,6 @@ app.delete("/rentals/:id", async (req, res) => {
   }
 });
 
-
-
 app.listen(4000, () => {
   console.log('Server is listening on port 4000.');
 });
-
-
-
-
-
-
-
-
-
-
-
-/* 
-app.get('/products', async (req, res) => {
-  try {
-    // buscando todos os produtos
-    const products = await connection.query(
-      `SELECT products.*, categories.name AS categoryName FROM products JOIN categories ON products."categoryId" = categories.id`
-    );
-
-    res.send(products.rows);
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
-});
-*/
